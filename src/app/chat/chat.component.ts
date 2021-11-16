ee97064eee0aba2dd3d38ab43cba2ebee0912fb8
Julio Cesar <s_integraciones.cetad@epfac.edu.co>
2021-11-16 10:58:42 -0500

    Notificar cuando un nuevo usuario se conecta al chat

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -35,7 +35,11 @@ export class ChatComponent implements OnInit {
         mensaje.fecha = new Date(mensaje.fecha);
         this.mensajes.push(mensaje);
         console.log(mensaje);
-      } )
+      });
+
+      this.mensaje.tipo = "NUEVO_USUARIO";
+      this.client.publish({destination: "/app/mensaje", body: JSON.stringify(this.mensaje)});
+
     }
 
     this.client.onDisconnect = (frame) => {
@@ -54,6 +58,7 @@ export class ChatComponent implements OnInit {
   }
 
   enviarMensaje():void{
+    this.mensaje.tipo = "MENSAJE";
     this.client.publish({destination: "/app/mensaje", body: JSON.stringify(this.mensaje)});
     this.mensaje.texto = '';    
   }
